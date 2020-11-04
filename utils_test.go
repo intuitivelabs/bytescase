bf4a61bc3e9b6a27100ab54074078df23be360cb
Andrei Pelinescu-Onciul <andrei@iptel.org>
2020-11-04 17:52:30 +0100

    copyright notice and reference to the license

--- utils_test.go
+++ utils_test.go
@@ -1,3 +1,9 @@
+// Copyright 2019-2020 Intuitive Labs GmbH. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree.
+
 package bytescase
 
 /* Functions to test against (used only when testing).
